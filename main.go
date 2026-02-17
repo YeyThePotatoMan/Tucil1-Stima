402cb3da600fcfaac8645cecea7304ece5a6fafc
YeyThePotatoMan <klocesaragih@gmail.com>
2026-02-17 21:56:28 +0700

    add: read file input

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main() {
-	fmt.Scan(&n)
-	g = make([]string, n)
-	ans = make([]int, n)
-
-	for i := 0; i < n; i++ {
-		fmt.Scan(&g[i])
+func read_file(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
 	}
+
+	sc := bufio.NewScanner(f)
+	g = []string{}
+	for sc.Scan() {
+		cnt += 1
+		line := sc.Text()
+		if len(line) > 0 {
+			g = append(g, line)
+		}
+	}
+	// TODO : add validation
+
+	n = len(g)
+	ans = make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	return true
+}
+
+func main() {
+	var path string
+	fmt.Scan(&path)
+
+	read_file(path)
+	// fmt.Scan(&n)
+
+	// for i := 0; i < n; i++ {
+	// 	fmt.Scan(&g[i])
+	// }
 
 	mask := make(map[uint8]bool)
 	solve2(0, false, mask)
